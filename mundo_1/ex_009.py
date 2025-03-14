451311c62f7418708639905030849d6eaa4cbdd2
ManuFlorenca! <manuelaflorenca22@gmail.com>
2025-03-14 15:03:39 -0300

    Reajustando ex 09

--- mundo_1/ex_009.py
+++ mundo_1/ex_009.py
@@ -2,9 +2,7 @@
 
 i = int(input('Digite um número para ver a sua tabuada: '))
 
-multiplicador = 1  # Começa multiplicando por 1
-
-while multiplicador <= 10:
+for multiplicador in range(1, 11):
     resultado = i * multiplicador
     print(f'{i} x {multiplicador} = {resultado}')
-    multiplicador += 1  # Incrementa o multiplicador para evitar loop infinito
+
