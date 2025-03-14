8d428f0dcdb43cb5689f0c4cf345301db4842205
ManuFlorenca! <manuelaflorenca22@gmail.com>
2025-03-14 14:11:41 -0300

    Primeiro ex

---
+++ mundo_1/ex_006.py
@@ -0,0 +1,10 @@
+n = float(input("Digite um número: "))
+
+dobro = n*2
+print(f"O dobro de {n} vale {dobro:.2f}.")
+
+triplo = n*3
+print(f"O triplo de {n} vale {triplo:.2f}.")
+
+r_quadrada = n**0.5
+print(f"A raiz quadrada de {n} vale {r_quadrada:.2f}.")
