3a68f20bf1ccfcc3f96fd956180316c59a7af6f6
ManuFlorenca! <manuelaflorenca22@gmail.com>
2025-03-17 13:04:38 -0300

    Primeiro ex facul

---
+++ atividades_facul/calculo_expressoes.py
@@ -0,0 +1,11 @@
+# Escreva um programa que peça ao usuário dois números inteiros x e y e calcule o seguinte:
+#z = x + y * 2
+# O programa deve exibir o resultado no formato:
+# O resultado da expressão é: [resultado]
+
+x = int(input('Escreva um número inteiro: '))
+y = int(input('Escreva outro número inteiro: '))
+
+z = (x+y)*2
+
+print(f'O resultado da expressão é: {z}')
