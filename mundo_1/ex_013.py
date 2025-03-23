0c297b6e7e8f2c1dff07ca5fa875540e5a95a7c7
ManuFlorenca! <manuelaflorenca22@gmail.com>
2025-03-22 23:32:28 -0300

    Exercicio 13

---
+++ mundo_1/ex_013.py
@@ -0,0 +1,7 @@
+# Reajuste salarial de 15%
+
+salario = float(input('Qual é o salário do Funcionário? R$'))
+
+reajuste = salario + (salario*15/100)
+
+print(f'Esse funcionário ganhava R${salario} mas agora com o reajuste passa a receber um aumento de 15% de R${reajuste}')
