0a3e9b106e24c00d11889755fac9406d19e6e69c
ManuFlorenca! <manuelaflorenca22@gmail.com>
2025-04-10 16:25:51 -0300

    exercicio 18

---
+++ mundo_1/ex_018.py
@@ -0,0 +1,13 @@
+# Seno, Cosseno e tangente
+import math
+
+angulo = float(input('Digite o ângulo que você deseja: '))
+rad = math.radians(angulo)
+
+seno = math.sin(rad)
+cosseno = math.cos(rad)
+tangente = math.tan(rad)
+
+print(f'O ângulo de {angulo} tem o SENO de {seno:.2f}')
+print(f'O ângulo de {angulo} tem o COSSENO de {cosseno:.2f}')
+print(f'O ângulo de {angulo} tem o TANGENTE de {tangente:.2f}')
