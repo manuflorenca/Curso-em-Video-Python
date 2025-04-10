0045419221d17890a5f3829593f83e3bd8fb5677
ManuFlorenca! <manuelaflorenca22@gmail.com>
2025-04-10 16:11:01 -0300

    Exercicio 17 feito

--- mundo_1/ex_017.py
+++ mundo_1/ex_017.py
@@ -1,1 +1,9 @@
-# Catetos e 
+# Catetos e hipotenusa
+
+import math
+
+cateto_oposto = float(input('Comprimento do cateto oposto: '))
+cateto_adjacente = float(input('Comprimento do cateto adjacente: '))
+
+resultado = math.hypot(cateto_oposto, cateto_adjacente)
+print(f'A hipotenusa vai medir {resultado:.2f}' )
